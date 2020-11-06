4f09b57d15250dac0f8d5bdb0f1651f6f7d223b3
1seulkii <1seulkii@naver.com>
2020-11-06 19:49:13 +0900

    로그인 가입시 로그인 아이디 중복체크

--- src/main/java/com/sbs/example/lolHi/controller/usr/MemberControllor.java
+++ src/main/java/com/sbs/example/lolHi/controller/usr/MemberControllor.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.example.lolHi.service.MemberService;
+import com.sbs.example.lolHi.util.Util;
 
 @Controller
 public class MemberControllor {
@@ -23,6 +24,19 @@ public class MemberControllor {
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public String doJoin(@RequestParam Map<String, Object> param) {
+		String loginId = Util.getAsStr(param.get("loginId"), "");
+		
+		if (loginId.length() == 0) {
+			return String.format(
+					"<script> alert('로그인 아이디를 입력해주세요.'); history.back(); </script>");
+		}
+		boolean isJoinAvailableLoginId = memberService.isJoinAvailableLoginId(loginId);
+
+		if (isJoinAvailableLoginId == false) {
+			return String.format(
+					"<script> alert('%s(은)는 이미 사용중인 아이디입니다.'); history.back(); </script>", loginId);
+		}
+		
 		int id = memberService.join(param);
 		
 		return String.format(
